6551625734d33ed2729df4ddb5a3894333646f57
nicolasnbl <noblet.nicolas07@gamil.com>
2021-11-22 22:52:09 +0100

    Message

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./../styles/components/Photo.scss"
 
 const Photo = () => {
 
     return (
         <div className="Photo">
-            
+            <img src="./img/photo-de-profil.jpg" alt="" />
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Photo from '../components/Photo';
+import "./../styles/pages/Home.scss"
 
 const Home = () => {
 
 
     return (
         <div className="Home">
-
+            <Photo />
         </div>
     );
 };
