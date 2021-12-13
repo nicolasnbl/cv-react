86c01e11fb4432cbe982c88fa167f94d1a6b5907
nicolasnbl <noblet.nicolas07@gamil.com>
2021-12-14 00:19:52 +0100

    Message je bloque sur les fonctions et les events lorsqu'il y a une entrée la fonction se lance au load et après ne fonctionne plus rechercher pourquoi

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,92 @@
-import React from 'react';
+import React, {useState, useEffect, Component} from 'react';
+import { MdMenu } from 'react-icons/md';
 
 const Navigation = () => {
+
+    const [isOpen, setisOpen] = useState(false);
+
+    const className = isOpen ? 'navOpen '+"Navigation" : 'navClose '+"Navigation";
+
+    const clickBtn = () => {
+        setisOpen(!isOpen);
+    }
+
+    const toggleClose = () => {
+        setisOpen(false);
+    }
+
+    const toggleOpen = () => {
+        setisOpen(true);
+    }
+
+    const scrollToElement = () => {
+        window.scrollTo({
+            top: 700,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+    
+
+
+    useEffect(() => {
+        const menu = document.getElementById("menu-nav");
+        const icon = document.getElementById("icon-nav");
+        const experience = document.getElementById("nav-experience");
+        menu.addEventListener('mouseleave', toggleClose);
+        icon.addEventListener('mouseenter', toggleOpen);
+        experience.addEventListener('click', scrollToElement);
+        return () => {
+            menu.removeEventListener('mouseleave', toggleClose);
+            icon.removeEventListener('mouseenter', toggleOpen);
+        }
+    }, [])
+
+
     return (
-        <div className="Navigation">
-            <div className="btn-nav">
-                <div className="trait"></div>
-                <div className="trait"></div>
-                <div className="trait"></div>
-                <div className="trait"></div>
+        <div className={className} id="menu-nav">
+
+            <div className="main-container-nav">
+                <div className="nav-experience nav-block">
+                    <div className="nav-experience-titre nav-titre" id='nav-experience'>
+                        Expériences
+                    </div>
+                    <div className="nav-sous-block-container">
+                        <div className="nav-stage nav-sous-block">
+                            Stage
+                        </div>
+                        <div className="nav-formation nav-sous-block">
+                            Formation
+                        </div>
+                        <div className="nav-divers nav-sous-block">
+                            Divers Expériences
+                        </div>
+                    </div>
+                </div>
+                <div className="nav-langue nav-block">
+                    <div className="nav-langue-titre nav-titre">
+                        Maîtrise des langues
+                    </div>
+                </div>
+                <div className="nav-competence nav-block">
+                    <div className="nav-competence-titre nav-titre">
+                        Compétence
+                    </div>
+                    <div className="nav-sous-block-container">
+                        <div className="nav-stage nav-sous-block">
+                            Conception assiter par ordinateur
+                        </div>
+                        <div className="nav-formation nav-sous-block">
+                            Programmation
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            <div type='button' id="icon-nav" onClick={clickBtn} className="icon-nav-container">
+                <MdMenu size='75px' />
+            </div>
+
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,75 +1,14 @@
 import React, { useEffect } from 'react';
-import BlockCompetence from '../components/BlockCompetence';
 import BlockContact from '../components/BlockContact';
-import BlockExperience from '../components/BlockExperience';
-import BlockInfos from '../components/BlockInfos';
-import BlockLangue from '../components/BlockLangue';
 import BtnScroll from '../components/BtnScroll';
 import BtnTelechargement from '../components/BtnTelechargement';
+import MainBlock from '../components/MainBlock';
 import Navigation from '../components/Navigation';
 import Photo from '../components/Photo';
+import Titre1 from '../components/Titre1';
 
 const Home = () => {
 
-    /*
-
-    var blockContainer = null;
-    var photoContainer = null;
-    var leftBlock = null;
-    var btnScrollContainer = null;
-
-
-    window.addEventListener("load", () =>{
-        blockContainer = document.getElementById("main-block");
-        photoContainer = document.getElementById("photo-block");
-        leftBlock = document.getElementById("block-contact-container");
-        btnScrollContainer = document.getElementById("btnScroll-container"); 
-    });
-
-    window.addEventListener("scroll", () =>{
-        const scroll = window.scrollY;
-
-        if( scroll > 0 ) {
-            scrollZeroCss();
-        }
-        else{
-            scrollPlusDeZeroCss();
-        }
-
-        if( scroll > 300 ) {
-            scrollplus300css(); //action css sans la var scroll
-        }
-        else {
-            scrollmoins300css(scroll);
-        }
-    });
-
-    const scrollZeroCss = () => {
-        btnScrollContainer.style.display = "none";
-    }
-
-    const scrollPlusDeZeroCss = () => {
-
-        btnScrollContainer.style.display = "block";
-    }
-    
-    const scrollplus300css = () => {
-        blockContainer.style.opacity = "1" ;
-        leftBlock.style.transform = "translateX(0%)";
-        btnScrollContainer.style.display = "none";
-    }
-
-    const scrollmoins300css = (scrl) => {
-        btnScrollContainer.style.display = "flex";
-        leftBlock.style.transform = "translateX(-120%)";
-        blockContainer.style.opacity =""+(scrl/300)+"";
-    }
-
-    const clickBtn = () => {
-        window.scroll(0, 300);
-    }
-
-    */
 
     return (
         <div className="Home">
@@ -86,6 +25,10 @@ const Home = () => {
                 <BtnTelechargement />
             </div>
 
+            <div className="titre1-container">
+                <Titre1 />
+            </div>
+
             <div className="photo-container" id="photo-block">
                 <Photo />
             </div>
@@ -94,20 +37,7 @@ const Home = () => {
                 <BtnScroll />
             </div>
 
-            <div id="main-block" className="block-container" >
-                <div className="block-infos-container sous-block">
-                    <BlockInfos />
-                </div>
-                <div className="block-experience-container sous-block">
-                    <BlockExperience />
-                </div>
-                <div className="block-langue-container sous-block">
-                    <BlockLangue />
-                </div>
-                <div className="block-competence-container sous-block">
-                    <BlockCompetence />
-                </div>
-            </div>
+            <MainBlock />
 
             <div className="block-left-container" id="block-left-container">
                 <div className="block-contact-container" id="block-contact-container">
