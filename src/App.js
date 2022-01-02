76d5475fe2b496df20b8de9200b6a4eee641ee02
nicolasnbl <noblet.nicolas07@gamil.com>
2022-01-02 01:37:38 +0100

    Message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import Home from './pages/Home';
+import { IconContext } from 'react-icons'
+
 
 function App() {
   return (
+    
     <div className="App">
+      <IconContext.Provider value={{ style: {verticalAlign: 'middle' }}}>
       <Home />
+      </IconContext.Provider>
     </div>
   );
 }
